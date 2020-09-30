ee232b88fcfdd3d134a7a8f96d69fc007a609d65
unknown <cuong1998pro@gmail.com>
2020-10-01 02:56:21 +0700

    merge conflig

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dangnhap', function () {
   return view('backend.dangnhap');
 });
-<<<<<<< HEAD
-
 Route::get('/dashboard', function () {
   return view('backend.dashboard');
 });
-
-Route::get('/news-feed', function () {
-  return view('backend.bantin');
-});
-=======
-Route::get('/admin',function (){
-    return view('dashboard.menu.index');
-});
-Route::view('/admin/home','dashboard.home.index');
->>>>>>> 9062036e0b3e7fb598fcba8432ba4cb1d5f9d67d
+// Route::view('/admin/home', 'dashboard.home.index');
