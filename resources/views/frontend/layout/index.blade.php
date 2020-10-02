3e501df8a7b5d4490295afe9246deda3261a657c
unknown <cuong1998pro@gmail.com>
2020-10-02 18:01:08 +0700

    toi uu

--- resources/views/frontend/layout/index.blade.php
+++ resources/views/frontend/layout/index.blade.php
@@ -32,7 +32,6 @@
   <script type="text/javascript" src="{{asset('assets/js/jquery.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('assets/js/jquery-ui.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-  <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 
 </body>
 
