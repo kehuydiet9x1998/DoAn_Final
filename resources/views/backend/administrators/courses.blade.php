5f404b0717ed6ff168b707904943fb85309a90fa
unknown <nguyenlinh1998adhp@gmail.com>
2020-10-03 10:09:09 +0700

    update modal form

--- resources/views/backend/administrators/courses.blade.php
+++ resources/views/backend/administrators/courses.blade.php
@@ -15,7 +15,7 @@
                     <div class="col-xs-12 col-sm-12 col-md-6">
                       <div id="dom-table_filter" class="dataTables_filter" style="margin-left: -145px;">
                         <label style="display: flex">Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="dom-table" style="width: 250px; height: 25px;margin-top: 0px;margin-left: 10px;">
-                          <button class="btn btn-success btn-round waves-effect waves-light" style="margin-top: -6px;height: 35px;line-height: 13px; margin-left: 5px">Thêm khóa học</button>
+                          <button type="button" class="btn btn-primary waves-effect" data-toggle="modal" data-target="#large-Modal" style="height: 26px; align-content: center; padding: 0px 10px;">Thêm khóa học</button>
                         </label>
                       </div>
                     </div>
@@ -169,4 +169,73 @@
       </div>
     </div>
   </div>
+  <div class="modal fade" id="large-Modal" tabindex="-1" role="dialog" style="z-index: 1050; display: none;" aria-hidden="true">
+    <form id="main" method="post" action="https://colorlib.com/" onsubmit="return false" novalidate="">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Thêm khóa học</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label">Simple Input</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="name" id="name" placeholder="Text Input Validation">
+                <span class="messages"></span>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label">Password</label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password input">
+                <span class="messages"></span>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label">Repeat Password</label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" id="repeat-password" name="repeat-password" placeholder="Repeat Password">
+                <span class="messages"></span>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label">Email</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="email" name="email" placeholder="Enter valid e-mail address">
+                <span class="messages"></span>
+              </div>
+            </div>
+            <div class="row">
+              <label class="col-sm-2 col-form-label">Radio Buttons</label>
+              <div class="col-sm-10">
+                <div class="form-check form-check-inline">
+                  <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="gender" id="gender-1" value="option1"> Male
+                  </label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="gender" id="gender-2" value="option2"> Female
+                  </label>
+                </div>
+                <span class="messages"></span>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default waves-effect " data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary m-b-0 waves-effect waves-light ">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+@endsection
+@section('script')
+  <script type="text/javascript" src="{{asset('assets/js/form-validation.js')}}"></script>
+  <script type="text/javascript" src="{{asset('assets/js/validate.js')}}"></script>
+  <script type="text/javascript" src="{{asset('assets/js/underscore-min.js')}}"></script>
 @endsection
--- resources/views/backend/layout/index.blade.php
+++ resources/views/backend/layout/index.blade.php
@@ -17,16 +17,6 @@
   <link rel="icon" href="https://colorlib.com/polygon/admindek/files/assets/images/favicon.ico" type="image/x-icon">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
-  <link rel="stylesheet" href="public/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- fullCalendar -->
-  <link rel="stylesheet" href="plugins/fullcalendar/main.min.css">
-  <link rel="stylesheet" href="plugins/fullcalendar-daygrid/main.min.css">
-  <link rel="stylesheet" href="plugins/fullcalendar-timegrid/main.min.css">
-  <link rel="stylesheet" href="plugins/fullcalendar-bootstrap/main.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="public/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
@@ -113,7 +103,7 @@
     <script type="text/javascript" src="{{asset('assets/js/calendar.js')}}"></script>
     <script src="{{asset('assets/js/rocket-loader.min.js')}}" data-cf-settings="d2d1d6e2f87cbebdf4013b26-|49" defer="">
     </script>
-
+  @yield('script')
 </body>
 
 </html>
