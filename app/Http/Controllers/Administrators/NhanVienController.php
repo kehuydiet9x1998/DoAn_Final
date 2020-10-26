d123caa95073ded6a09ed3fc56524e1f367f717c
NguyenVanHoan-HP-98 <vanhoan98.hp@gmail.com>
2020-10-26 08:47:00 +0700

    Hoàn thiên phan bai tap

--- app/Http/Controllers/Administrators/NhanVienController.php
+++ app/Http/Controllers/Administrators/NhanVienController.php
@@ -10,91 +10,90 @@ use Illuminate\Http\Request;
 
 class NhanVienController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $staffs = NhanVien::orderBy('ten', 'ASC')->paginate(10);
-        $chucvu =ChucVu::all();
-        return view('backend.administrators.staff.staffs', ['staffs' => $staffs,'chucvu'=>$chucvu]);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $staffs = NhanVien::orderBy('ten', 'ASC')->paginate(10);
+    $chucvu = ChucVu::all();
+    return view('backend.administrators.staff.staffs', ['staffs' => $staffs, 'chucvu' => $chucvu]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $data = $request->all();
-        NhanVien::create($data);
-        return redirect(route('staffs.index'));
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $data = $request->all();
+    NhanVien::create($data);
+    return redirect(route('staffs.index'));
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\NhanVien  $nhanVien
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $staffs = NhanVien::findorFail($id);
-        return view('backend.administrators.staff.show_staffs',['staffs' => $staffs]);
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\NhanVien  $nhanVien
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $staffs = NhanVien::findorFail($id);
+    return view('backend.administrators.staff.show_staffs', ['staffs' => $staffs]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\NhanVien  $nhanVien
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $data = NhanVien::find($id);
-        $chucvu = ChucVu::all();
-        return view('backend.administrators.staff.edit_staffs', ['staffs' => $data, 'chucvu' => $chucvu]);
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\NhanVien  $nhanVien
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $data = NhanVien::find($id);
+    $chucvu = ChucVu::all();
+    return view('backend.administrators.staff.edit_staffs', ['staffs' => $data, 'chucvu' => $chucvu]);
+  }
 
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\NhanVien  $nhanVien
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $nhanvien = NhanVien::findorFail($id);
+    $nhanvien->fill($request->all());
+    $nhanvien->save();
+    return redirect(route('staffs.index'));
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\NhanVien  $nhanVien
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $nhanvien = NhanVien::findorFail($id);
-        $nhanvien->fill($request -> all());
-        $nhanvien -> save();
-        return redirect(route('staffs.index'));
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\NhanVien  $nhanVien
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        NhanVien::where('id', '=', $id)->delete();
-        return redirect(route('staffs.index'));
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\NhanVien  $nhanVien
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    NhanVien::where('id', '=', $id)->delete();
+    return redirect(route('staffs.index'));
+  }
 }
--- app/Http/Controllers/Contacts/HocPhiController.php
+++ app/Http/Controllers/Contacts/HocPhiController.php
@@ -11,93 +11,92 @@ use Illuminate\Support\Facades\Redirect;
 
 class HocPhiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $hocphi = HocPhi::all();
-        $khoanthu = KhoanThu::all();
-        return view('backend.contact.hocphi.ListHocPhi',compact('khoanthu','hocphi'));
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $hocphi = HocPhi::all();
+    $khoanthu = KhoanThu::all();
+    return view('backend.contact.hocphi.ListHocPhi', compact('khoanthu', 'hocphi'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $data = $request->all();
-        $data['sotienconthieu'] = $data['sotiencandong'] - $data['sotiendadong'];
-        LichSuHocPhi::create($data);
-        return redirect(route('hocphis.index'));
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $data = $request->all();
+    $data['sotienconthieu'] = $data['sotiencandong'] - $data['sotiendadong'];
+    LichSuHocPhi::create($data);
+    return redirect(route('hocphis.index'));
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\HocPhi  $hocPhi
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\HocPhi  $hocPhi
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+  }
 
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\HocPhi  $hocPhi
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $hocphi = HocPhi::find($id);
+    $khoanthu = KhoanThu::all();
+    $lsthu =  LichSuHocPhi::all()->where('hoc_phi_id', $id);
+    $sotiencandong = LichSuHocPhi::where('hoc_phi_id', $id)->orderBy('created_at', 'desc')->first()->sotienconthieu;
+    return view(
+      'backend.contact.hocphi.Edit_HocPhi_Modal',
+      ['hocphi' => $hocphi, 'khoanthu' => $khoanthu, 'lsthu' => $lsthu, 'sotiencandong' => $sotiencandong]
+    );
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\HocPhi  $hocPhi
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $hocphi = HocPhi::find($id);
-        $khoanthu = KhoanThu::all();
-        $lsthu =  LichSuHocPhi::all()->where('hoc_phi_id',$id);
-        $sotiencandong = LichSuHocPhi::where('hoc_phi_id',$id)->orderBy('created_at', 'desc')->first()->sotienconthieu;
-        return view('backend.contact.hocphi.Edit_HocPhi_Modal',
-          ['hocphi'=>$hocphi,'khoanthu'=>$khoanthu,'lsthu'=>$lsthu, 'sotiencandong' => $sotiencandong]);
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\HocPhi  $hocPhi
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
 
-    }
+    $hocphi = HocPhi::find($id);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\HocPhi  $hocPhi
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request,$id)
-    {
-
-      $hocphi = HocPhi::find($id);
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\HocPhi  $hocPhi
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        HocPhi::Where('id','=',$id)->delete();
-        return redirect(route('hocphis.index'));
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\HocPhi  $hocPhi
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    HocPhi::Where('id', '=', $id)->delete();
+    return redirect(route('hocphis.index'));
+  }
 }
